663ddc70d0b1a4ad289189238266519c938624b1
Gui113893 <gui.santos91@ua.pt>
2024-02-19 15:54:23 +0000

    loadPuzzle

--- lab01/WPuzzle.java
+++ lab01/WPuzzle.java
@@ -1,5 +1,39 @@
 package lab01;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WPuzzle {
     
+    public static WPuzzle LoadFromFile(String filename) throws FileNotFoundException{
+        Scanner input = new Scanner(new FileReader(filename));
+        String line = input.nextLine();
+        int cols = line.length();
+        int rows = 0;
+        ArrayList<ArrayList<Character>> puzzleArray = new ArrayList<>();
+        String regex = "[A-Z]{" + cols + "}";
+        if (!line.matches(regex)){
+            input.close();
+            return null;
+        }
+        puzzleArray.add();
+        rows++;
+        
+        while (input.hasNextLine()){
+            //While rows < cols, keep reading
+            if (rows < cols){
+                line = input.nextLine();
+                if (line.length() != cols || !line.matches(regex)){
+                    input.close();
+                    return null;
+                }
+                puzzleArray.add();
+                rows++;
+            }
+        }
+        input.close();
+        return null;
+    }
 }

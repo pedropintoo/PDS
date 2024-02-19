19119ba07c05b63f0ee8c54df99954fc4cddcb7f
Pedro Pinto <pmap@ua.pt>
2024-02-19 19:28:05 +0000

    WPuzzleLoad - Create

--- lab01/WPuzzle.java
+++ lab01/WPuzzle.java
@@ -1,13 +1,8 @@
 package lab01;
 import java.util.ArrayList;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
-
 public class WPuzzle {
     
-    // [][]
     char[][] puzzleArray;
     ArrayList<String> targets;
 
@@ -16,34 +11,4 @@ public class WPuzzle {
         this.targets = targets;
     }
     
-
-    public static WPuzzle LoadFromFile(String filename) throws FileNotFoundException{
-        Scanner input = new Scanner(new FileReader(filename));
-        String line = input.nextLine();
-        int cols = line.length();
-        int rows = 0;
-        ArrayList<ArrayList<char>> puzzleArray = new ArrayList<>();
-        String regex = "[A-Z]{" + cols + "}";
-        if (!line.matches(regex)){
-            input.close();
-            return null;
-        }
-        puzzleArray.add();
-        rows++;
-        
-        while (input.hasNextLine()){
-            //While rows < cols, keep reading
-            if (rows < cols){
-                line = input.nextLine();
-                if (line.length() != cols || !line.matches(regex)){
-                    input.close();
-                    return null;
-                }
-                puzzleArray.add();
-                rows++;
-            }
-        }
-        input.close();
-        return null;
-    }
 }
---
+++ lab01/WPuzzleLoad.java
@@ -0,0 +1,40 @@
+package lab01;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+public class WPuzzleLoad {
+    
+    public static WPuzzle LoadFromFile(String filename) throws FileNotFoundException{
+        Scanner input = new Scanner(new FileReader(filename));
+        String line = input.nextLine();
+        int cols = line.length();
+        int rows = 0;
+        ArrayList<ArrayList<char>> puzzleArray = new ArrayList<>();
+        String regex = "[A-Z]{" + cols + "}";
+        if (!line.matches(regex)){
+            input.close();
+            return null;
+        }
+        puzzleArray.add();
+        rows++;
+        
+        while (input.hasNextLine()){
+            //While rows < cols, keep reading
+            if (rows < cols){
+                line = input.nextLine();
+                if (line.length() != cols || !line.matches(regex)){
+                    input.close();
+                    return null;
+                }
+                puzzleArray.add();
+                rows++;
+            }
+        }
+        input.close();
+        return null;
+    }
+
+}

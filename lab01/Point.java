893db19a292ded9f7dbffb0a6ddb094a822ec2ae
Pedro Pinto <pmap@ua.pt>
2024-02-20 16:53:29 +0000

    WSSolver - Touching Point In Direction

--- lab01/Point.java
+++ lab01/Point.java
@@ -2,9 +2,9 @@ package lab01;
 
 public class Point {
     // Only Positive coordinates
-    private int x;
-    private int y;
-    private int limit;
+    private final int x;
+    private final int y;
+    private final int limit;
 
     Point(int x, int y, int limit) {
         this.x = x;
@@ -14,12 +14,56 @@ public class Point {
 
     // Function that returns the nearby points
     public Point[] touchingPoints(){
+        
         return null;
     }
 
     // Function that returns the near point in a specific direction
-    public Point touchingPointsInDirection(WSDirection direction){
-        return null;
+    public Point touchingPointInDirection(WSDirection direction){
+        return getPointInDirection(direction);
+    }
+
+    // Get the points (if valid) in a direction
+    // Using this conditions:
+    //      UP-CONDITION -> y <= 0
+    //      RIGHT-CONDITION -> x >= limit-1
+    //      DOWN-CONDITION -> y >= limit-1
+    //      LEFT-CONDITION -> x <= 0
+    private Point getPointInDirection(WSDirection direction) {
+        Point point = null;
+        
+        switch (direction) {
+            case UP: 
+                if (y <= 0) break;                            // UP-CONDITION
+                point =  new Point(x,(y-1)%limit,limit);         
+            case RIGHT_UP: 
+                if (y <= 0) break;                            // UP-CONDITION
+                if (x >= limit-1) break;                      // RIGHT-CONDITION
+                point = new Point((x+1)%limit,(y-1)%limit,limit);
+            case RIGHT:
+                if (x >= limit-1) break;                      // RIGHT-CONDITION
+                point = new Point((x+1)%limit,y,limit);
+            case RIGHT_DOWN:
+                if (x >= limit-1) break;                      // RIGHT-CONDITION
+                if (y >= limit-1) break;                      // DOWN-CONDITION 
+                point = new Point((x+1)%limit,(y+1)%limit,limit);                
+            case DOWN:
+                if (y >= limit-1) break;                      // DOWN-CONDITION 
+                point = new Point(x,(y+1)%limit,limit);  
+            case LEFT_DOWN:
+                if (x <= 0) break;                            // LEFT-CONDITION
+                if (y >= limit-1) break;                      // DOWN-CONDITION     
+                point = new Point((x+1)%limit,(y+1)%limit,limit);
+            case LEFT:
+                if (x <= 0) break;                            // LEFT-CONDITION
+                point = new Point((x-1)%limit,y,limit);
+            case LEFT_UP:
+                if (x <= 0) break;                            // LEFT-CONDITION
+                if (y <= 0) break;                            // UP-CONDITION
+                point = new Point((x-1)%limit,(y-1)%limit,limit); 
+        }
+
+        return point;
     }
 
 }

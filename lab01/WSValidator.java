e4699dadf0d0a0aff45897b3caf8600385335c3c
Guilherme Santos <gui.santos91@ua.pt>
2024-02-22 16:49:48 +0000

    WSValidator | logic for filter done

--- lab01/WSValidator.java
+++ lab01/WSValidator.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class WSValidator {
     public static boolean validate(WSSolver wSolver) {
-
+        filter(wSolver);
         // Validate if every target was found and only one time
         for (Map.Entry<String, ArrayList<Vector>> targetsMap : wSolver.getTargets_map().entrySet()) {
             ArrayList<Vector> list_vectors = targetsMap.getValue();
@@ -16,4 +16,28 @@ public class WSValidator {
         return true;
 
     }
+
+    public static void filter(WSSolver wsSolver){
+        Map<String, ArrayList<Vector>> targets_map = wsSolver.getTargets_map();
+
+        for (String target : targets_map.keySet()){
+            for (String targetToCompare : targets_map.keySet()){
+                if (isTargetToCompareCandidate(target, targetToCompare)){
+                    ArrayList<Vector> targetVector = targets_map.get(target);
+                    ArrayList<Vector> targetToCompareVector = targets_map.get(targetToCompare);
+
+                    removeInvalidPoints(targetVector, targetToCompareVector);                    
+                }
+            }
+        } 
+    }
+
+    public static boolean isTargetToCompareCandidate(String target, String targetToCompare){
+        return !target.equals(targetToCompare) && targetToCompare.contains(target);
+    }
+
+    public static void removeInvalidPoints(ArrayList<Vector> targetVector, ArrayList<Vector> targetToCompareVector){
+        // TODO
+    }
+    
 }

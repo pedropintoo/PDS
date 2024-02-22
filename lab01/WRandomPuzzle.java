caed81a8c6a708a6c120deefeb471e40ee58baa8
Pedro Pinto <pmap@ua.pt>
2024-02-22 23:45:03 +0000

    Working version

--- lab01/WRandomPuzzle.java
+++ lab01/WRandomPuzzle.java
@@ -9,34 +9,26 @@ public class WRandomPuzzle {
     private final static int TRIES_BEFORE_RESIGN = (int) 1e10;
     private final static char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-    public WPuzzle generateRandomPuzzle(ArrayList<String> targets,int size) {
-        // simple condition
-        if (!isTargetsLengthValid(targets, size)) return null;
-
-        // generate a random puzzle
-        char[][] puzzleArray = generateRandomPuzzleArray(size);
-
-        // Try to insert targets in random positions in the puzzle
-        puzzleArray = insertTargetsInPuzzle(puzzleArray, targets, size);
-
-        // Check if the puzzle was created
-        if (puzzleArray == null) return null;
-
-        for (int i = 0; i < size; i++){
-            for (int j = 0; j < size; j++){
-                System.out.print(puzzleArray[i][j]);
-            }
-            System.out.println();
-        }
-
-        return new WPuzzle(puzzleArray,targets,size);
-    }
-
-    private char[][] insertTargetsInPuzzle(char[][] puzzleArray, ArrayList<String> targets, int size){
+    public static WPuzzle generateRandomPuzzle(ArrayList<String> targets,int size) {
         // [Not the best algorithm]
         // Algorithm: We will attempt to insert targets TRIES_BEFORE_RESIGN times in random positions.
         // Therefore, it is a probabilistic algorithm, and success depends on both the input and the value of TRIES_BEFORE_RESIGN.
 
+        // simple condition
+        if (!isTargetsLengthValid(targets, size)) return null;
+
+        // Try to insert targets in random positions in a random puzzle
+        char[][] puzzleArray = insertTargetsInPuzzle(targets, size);
+
+        // Check if the puzzle was created
+        if (puzzleArray == null) return null;
+
+        return new WPuzzle(puzzleArray,targets,size);
+    }
+
+    private static char[][] insertTargetsInPuzzle(ArrayList<String> targets, int size){
+
+        char[][] puzzleArray = null;
         Point current = null;
         int n_try = 0;
         int x = 0;
@@ -44,18 +36,22 @@ public class WRandomPuzzle {
 
         while (true) {   
             if (n_try++ > TRIES_BEFORE_RESIGN) return null; // Failure
+            
+            // generate a random puzzle
+            puzzleArray = generateRandomPuzzleArray(size);
 
+            // Try to fill with all the targets
             for (String target : targets) {
                 x = RANDOM.nextInt(size);
                 y = RANDOM.nextInt(size);
 
                 current = new Point(x, y, size);
-                puzzleArray[y][x] = target.charAt(0);
+                puzzleArray[y][x] = Character.toUpperCase(target.charAt(0));
 
-                // Chose a direction for the target
+                // Chose a direction for the specific target
                 WSDirection direction = WSDirection.randomDirection();
 
-                // Put the remaining characters in the position (try)
+                // Put the remaining characters in the direction (try)
                 for (int n = 1; n < target.length(); n++) {
 
                     current = current.getPointInDirection(direction);
@@ -66,7 +62,7 @@ public class WRandomPuzzle {
                     x = current.getX();
                     y = current.getY();
 
-                    puzzleArray[y][x] = target.charAt(n);
+                    puzzleArray[y][x] = Character.toUpperCase(target.charAt(n));
                 }
 
                 if (current == null) break; // TRIPLE-BREAK
@@ -81,7 +77,7 @@ public class WRandomPuzzle {
         return puzzleArray;
     }
 
-    private boolean isValidPuzzle(char[][] puzzleArray, ArrayList<String> targets, int size) {
+    private static boolean isValidPuzzle(char[][] puzzleArray, ArrayList<String> targets, int size) {
         // Check Valid by solving!
 
         WSSolver solver = new WSSolver(new WPuzzle(puzzleArray, targets, size));
@@ -89,7 +85,7 @@ public class WRandomPuzzle {
         return solver.getState() == WSState.SOLVED;
     }
 
-    private boolean isTargetsLengthValid(ArrayList<String> targets, int size){
+    private static boolean isTargetsLengthValid(ArrayList<String> targets, int size){
         for (String target : targets) {
             if (target.length() > size) return false;
         }
@@ -98,7 +94,7 @@ public class WRandomPuzzle {
     }
 
     // Generates a random letter in each cell of puzzle array
-    private char[][] generateRandomPuzzleArray(int size) {
+    private static char[][] generateRandomPuzzleArray(int size) {
         char[][] puzzleArray = new char[size][size];
 
         for (int i = 0; i < size; i++) {
--- lab01/WSGenerator.java
+++ lab01/WSGenerator.java
@@ -13,8 +13,19 @@ public class WSGenerator {
 
         ArrayList<String> targets = new ArrayList<>();
         targets.add("programming");
+        targets.add("flask");
+        targets.add("zzzzzzzzz");
+        targets.add("zzzaaaazzz");
 
-        
+        WPuzzle puzzle = WRandomPuzzle.generateRandomPuzzle(targets, size);
+
+        System.out.println("Puzzle:");
+        for (int i = 0; i < puzzle.getSize(); i++){
+            for (int j = 0; j < puzzle.getSize(); j++){
+                System.out.print(puzzle.getPuzzleArray()[i][j]);
+            }
+            System.out.println();
+        }
 
 
     }

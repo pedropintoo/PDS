3209588403b30917ae82b2222ceb3c961c32adf1
Pedro Pinto <pmap@ua.pt>
2024-02-24 17:39:59 +0000

    Add spaces between draw

--- lab01/WPuzzle.java
+++ lab01/WPuzzle.java
@@ -31,7 +31,7 @@ public class WPuzzle {
         System.out.println();
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.print(puzzleArray[i][j]);
+                System.out.print(puzzleArray[i][j] + " ");
             }
             System.out.println();
         }

43f4a0caa289944d161d7b5544d1f80304fcde62
Pedro Pinto <pmap@ua.pt>
2024-03-04 16:09:17 +0000

    nothing

--- lab03/flight/Flight.java
+++ lab03/flight/Flight.java
@@ -1,11 +1,27 @@
 package lab03.flight;
 
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Flight {
 
-    private int FlightCode;
+    private boolean hasExclusive;
+    private int rowsTourists;
+    private int colsTourists;
+    private int flightCode;
 
-    public Flight(String fileName) {
-        
+    public Flight(int flightCode, int rowsTourists, int colsTourists) {
+        this.rowsTourists = rowsTourists;
+        this.colsTourists = colsTourists;
+        this.hasExclusive = false;
     }
+
+    public public Flight(int flightCode, int rowsTourists, int colsTourists) {
+        this.rowsTourists = rowsTourists;
+        this.colsTourists = colsTourists;
+        this.hasExclusive = false;
+    }
+
+    loader
     
 }

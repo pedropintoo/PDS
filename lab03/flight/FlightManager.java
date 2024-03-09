f9f7c52b313fdc4c4e7ae009f2f215db3d40a5fe
Pedro Pinto <pmap@ua.pt>
2024-03-09 15:10:48 +0000

    start structure

---
+++ lab03/flight/FlightManager.java
@@ -0,0 +1,18 @@
+package lab03.flight;
+
+import java.util.HashSet;
+
+public class FlightManager {
+
+    private HashSet<Flight> fleetOfPlanes;
+
+    public FlightManager() {
+        fleetOfPlanes = new HashSet<>();
+    }
+
+    public void addFlightByFilename(String filename){
+        
+    }
+    
+}
+
---
+++ lab03/flight/Main.java
@@ -0,0 +1,5 @@
+package lab03.flight;
+
+public class Main {
+    FlightManager fm = new FlightManager();
+}

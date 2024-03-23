5f5ade1d9be88398410ea1a27983745d14968b53
Pedro Pinto <pmap@ua.pt>
2024-03-23 17:26:34 +0000

    small changes

--- lab05/AluguerAuto/Ligeiro/Eletrico.java
+++ lab05/AluguerAuto/Ligeiro/Eletrico.java
@@ -1,3 +1,8 @@
+/**
+ * @ Author: Pedro Pinto (pmap@ua.pt) & Guilherme Santos (gui.santos91@ua.pt)
+ * @ Create Time: 2024-03-18
+ */
+
 package AluguerAuto.Ligeiro;
 
 import AluguerAuto.Interfaces.VeiculoEletrico;
--- lab05/PagaLeva/Container/Container.java
+++ lab05/PagaLeva/Container/Container.java
@@ -1,3 +1,8 @@
+/**
+ * @ Author: Pedro Pinto (pmap@ua.pt) & Guilherme Santos (gui.santos91@ua.pt)
+ * @ Create Time: 2024-03-18
+ */
+
 package PagaLeva.Container;
 
 import PagaLeva.Food.Portion;

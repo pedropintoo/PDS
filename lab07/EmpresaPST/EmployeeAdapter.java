ee68086d55d1c88b9b1e8352d27ccb7e01e53cb6
Pedro Pinto <pmap@ua.pt>
2024-04-19 12:10:35 +0100

    lab07 comments + diagrams

--- lab07/EmpresaPST/EmployeeAdapter.java
+++ lab07/EmpresaPST/EmployeeAdapter.java
@@ -1,3 +1,8 @@
+/**
+ * @ Author: Pedro Pinto (pmap@ua.pt) & Guilherme Santos (gui.santos91@ua.pt)
+ * @ Create Time: 2024-04-19
+ */
+
 package EmpresaPST;
 
 import EmpresaPST.Petiscos.Empregado;
@@ -11,15 +16,18 @@ public class EmployeeAdapter extends Employee{
         this.empregado = empregado;
     }
 
+    @Override
     public String getName() {
         return this.getName();
     }
 
+    @Override
     public long getEmpNum() {
         long empNum = empregado.codigo();
         return empNum;
     }
 
+    @Override
     public double getSalary() {
         return empregado.salario();
     }

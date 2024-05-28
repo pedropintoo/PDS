80644638c09c554d70dbde5842258ca510ef4ad0
Guilherme Santos <gui.santos91@ua.pt>
2024-05-28 20:21:23 +0100

    ex3 scanner fix

--- lab12/ex3/Main.java
+++ lab12/ex3/Main.java
@@ -13,53 +13,53 @@ public class Main {
         books.add(book1);
         books.add(book2);
         books.add(book3);
+        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("*** Library ***");
-            for (Book book : books) {
-                System.out.println(book);
-            }
-            System.out.println(">> <book>, <operation: (1)register, (2)require, (3)return, (4)reserve, (5)cancel (6)quit>");
-
-            System.out.print(">> ");
-            Scanner scanner = new Scanner(System.in);
-            String[] input = scanner.nextLine().split(",");
-            int bookISBN = Integer.parseInt(input[0]);
-            int operation = Integer.parseInt(input[1]);
-
-            if (operation == 6) {
-                scanner.close();
-                break;
-            }
-
-            // Book exists?
-            Book book = books.stream().filter(b -> b.getISBN() == bookISBN).findFirst().orElse(null);
-            if (book == null) {
-                System.out.println("Book not found");
-                continue;
-            }
-            else {
-                switch (operation) {
-                    case 1:
-                        book.registerBook();
-                        break;
-                    case 2:
-                        book.requireBook();
-                        break;
-                    case 3:
-                        book.returnBook();
-                        break;
-                    case 4:
-                        book.reserveBook();
-                        break;
-                    case 5:
-                        book.cancelBook();
-                        break;
-                    default:
-                        System.out.println("Invalid operation");
-                        break;
+        try {
+            while (true) {
+                System.out.println("*** Library ***");
+                for (Book book : books) {
+                    System.out.println(book);
+                }
+                System.out.println(">> <book>, <operation: (1)register, (2)require, (3)return, (4)reserve, (5)cancel>");
+    
+                System.out.print(">> ");
+                String[] input = scanner.nextLine().split(",");
+                int bookISBN = Integer.parseInt(input[0]);
+                int operation = Integer.parseInt(input[1]);
+    
+                // Book exists?
+                Book book = books.stream().filter(b -> b.getISBN() == bookISBN).findFirst().orElse(null);
+                if (book == null) {
+                    System.out.println("Book not found");
+                    continue;
+                }
+                else {
+                    switch (operation) {
+                        case 1:
+                            book.registerBook();
+                            break;
+                        case 2:
+                            book.requireBook();
+                            break;
+                        case 3:
+                            book.returnBook();
+                            break;
+                        case 4:
+                            book.reserveBook();
+                            break;
+                        case 5:
+                            book.cancelBook();
+                            break;
+                        default:
+                            System.out.println("Invalid operation");
+                            break;
+                    }
                 }
             }
+        } finally {
+            System.out.println("Closing");
+            scanner.close();
         }
     }
 }

9b5bac9c8b457ce46fa1798aa8ce20267951e0e4
Guilherme Santos <gui.santos91@ua.pt>
2024-05-28 15:31:12 +0100

    insertion sort

--- lab12/ex1/InsertionSort.java
+++ lab12/ex1/InsertionSort.java
@@ -1,5 +1,32 @@
 package ex1;
 
-public class InsertionSort{
-    
+import java.util.Comparator;
+import java.util.List;
+
+public class InsertionSort<T> extends Algorithm<T>{
+
+    public InsertionSort(List<T> list, Comparator<T> comparator) {
+        super(list, comparator);
+    }
+
+    public static <T> void insertionSort(List<T> list, Comparator<T> comparator) {
+        int n = list.size();
+        for (int i = 1; i < n; i++) {
+            T key = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && comparator.compare(list.get(j), key) > 0) {
+                list.set(j + 1, list.get(j));
+                j = j - 1;
+            }
+            list.set(j + 1, key);
+        }
+    }
+
+    @Override
+    public void concreteSort() {
+        insertionSort(super.getList(), super.getComparator());
+    } 
+
 }
+

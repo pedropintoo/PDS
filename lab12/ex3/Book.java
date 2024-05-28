e2afe065e00bbafd6bd7d4758f7fa7032cf743d3
Guilherme Santos <gui.santos91@ua.pt>
2024-05-28 15:41:56 +0100

    book

--- lab12/ex3/Book.java
+++ lab12/ex3/Book.java
@@ -1,5 +1,27 @@
 package ex3;
 
 public class Book {
-    
+    private static int idCounter = 0;
+    private String title;
+    private int ISBN;
+    private int ano;
+    private String author;
+    private State state;
+
+    public Book(String title, int ano, String author) {
+        this.title = title;
+        this.ISBN = idCounter++;
+        this.ano = ano;
+        this.author = author;
+        this.state = new InventoryState(this);
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-4d %-10s %-10s [%-10s]", ISBN, title, author, state.toString());
+    }
 }
